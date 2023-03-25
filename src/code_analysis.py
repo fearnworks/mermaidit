987c9005697024e11a2a8f3bd8dd64f1e51c3445
jphillips <josh.phillips@fearnworks.com>
2023-03-25 13:37:39 -0500

    Add parsing of src class files

--- src/code_analysis.py
+++ src/code_analysis.py
@@ -7,21 +7,31 @@ from mermaid_parser import MermaidParser
 def analyze_code_base(local_path, output_dir=None):
     for root, _, files in os.walk(local_path):
         for file in files:
-            if file.endswith(".py"):
+            if file.endswith('.py'):
                 file_path = os.path.join(root, file)
-                mermaid_parser = MermaidParser()
-                mermaid_parser.parse_file(file_path)
-                mermaid_diagram = mermaid_parser.get_diagram()
-                wrapped_mermaid_diagram = wrap_mermaid_code(mermaid_diagram)
+                abs_file_path = os.path.abspath(file_path)
+                print(f"Processing: {abs_file_path}")
 
-                if output_dir:
-                    output_file_path = os.path.join(
-                        output_dir, os.path.splitext(file)[0] + ".md"
-                    )
-                else:
-                    output_file_path = os.path.splitext(file_path)[0] + ".md"
+                try:
+                    mermaid_parser = MermaidParser()
+                    mermaid_parser.parse_file(file_path)
+                    mermaid_diagram = mermaid_parser.get_diagram()
+                    
+                    # Check if the diagram contains any class definitions
+                    if "class " not in mermaid_diagram:
+                        print(f"No classes found in {file}, skipping.")
+                        continue
 
-                with open(output_file_path, "w") as f:
-                    f.write(wrapped_mermaid_diagram)
+                    wrapped_mermaid_diagram = wrap_mermaid_code(mermaid_diagram)
 
-                print(f"Mermaid diagram for {file} saved at {output_file_path}")
+                    if output_dir:
+                        output_file_path = os.path.join(output_dir, os.path.splitext(file)[0] + '.md')
+                    else:
+                        output_file_path = os.path.splitext(file_path)[0] + '.md'
+
+                    with open(output_file_path, 'w') as f:
+                        f.write(wrapped_mermaid_diagram)
+
+                    print(f"Mermaid diagram for {file} saved at {output_file_path}")
+                except UnicodeDecodeError as e:
+                    print(f"Error processing {abs_file_path}: {e}")
--- src/mermaid_parser.py
+++ src/mermaid_parser.py
@@ -6,17 +6,31 @@ class MermaidParser:
         self.class_diagram = "classDiagram\n"
 
     def parse_file(self, file_path):
-        with open(file_path, "r") as f:
+        with open(file_path, 'r') as f:
             content = f.read()
             self.parse_classes(content)
 
     def parse_classes(self, content):
-        class_definitions = re.findall(
-            r"^class\s+(\w+)(\s*\((\w+)\))?:", content, re.MULTILINE
-        )
+        class_definitions = re.findall(r'^class\s+(\w+)(\s*\((\w+)\))?:', content, re.MULTILINE)
 
         for class_def in class_definitions:
             class_name, _, base_class = class_def
+            self.class_diagram += f"class {class_name} {{\n"
+            
+            methods = re.findall(r'^\s+def\s+(\w+)\s*\((.*?)\)(\s*->\s*(\w+))?:', content, re.MULTILINE)
+            for method in methods:
+                method_name, params, _, return_type = method
+                if return_type:
+                    self.class_diagram += f"    +{method_name}({params}) : {return_type}\n"
+                else:
+                    self.class_diagram += f"    +{method_name}({params})\n"
+
+            properties = re.findall(r'^\s+([a-zA-Z_]\w+)\s*=', content, re.MULTILINE)
+            for prop in properties:
+                self.class_diagram += f"    +{prop}\n"
+
+            self.class_diagram += "}\n"
+
             if base_class:
                 self.class_diagram += f"{base_class} <|-- {class_name}\n"
 
