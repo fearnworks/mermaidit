9aa4c73fac48bb8f252223e5ec0e0a251ac2df40
jphillips <josh.phillips@fearnworks.com>
2023-03-25 14:38:11 -0500

    Remove tk tests

--- src/tests/test_file_operations.py
+++ src/tests/test_file_operations.py
@@ -9,14 +9,6 @@ DEFAULT_DATA_DIR = os.path.join(os.path.abspath("."), "data")
 SETTINGS_FILE = os.path.join(DEFAULT_DATA_DIR, "settings.txt")
 
 
-def test_browse_directory(tmp_path):
-    with patch(
-        "src.file_operations.filedialog.askdirectory", return_value=str(tmp_path)
-    ):
-        result = browse_directory()
-        assert result == str(tmp_path)
-
-
 def test_save_and_load_settings():
     save_settings(1, "/test/output/dir")
     settings = load_settings()
